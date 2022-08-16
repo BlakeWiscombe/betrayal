b5d15199f2bd77614822dd2fa1d7e48e88366bdf
BlakeWiscombe <2011264@schoolsnet.act.edu.au>
2022-08-16 16:30:39 +1000

    update

--- ChangeScene.gd
+++ ChangeScene.gd
@@ -5,7 +5,7 @@ export(String) var scene_to_load
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_tree().change_scene(scene_to_load)
+	get_tree().change_scene("res://Main menu.tscn")
 
 
 func _on_Button_pressed():
