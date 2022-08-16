2da14c461c2e987bc0f5ef1d72d4441fb3eaa529
BlakeWiscombe <2011264@schoolsnet.act.edu.au>
2022-08-16 16:38:40 +1000

    scenes connect better

--- ChangeScene.gd
+++ ChangeScene.gd
@@ -5,8 +5,8 @@ export(String) var scene_to_load
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_tree().change_scene("res://Main menu.tscn")
+	pass 
 
 
 func _on_Button_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene("res://Main menu.tscn")
