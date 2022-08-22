e198eea271612b5ebde59c68417d7f9361bd6ac1
Blake <2011264@schoolsnet.act.edu.au>
2022-08-22 10:14:19 +1000

    added player mouse look

--- ChangeScene.gd
+++ ChangeScene.gd
@@ -3,8 +3,8 @@ extends Button
 export(String) var scene_to_load
 
 
-# Called when the node enters the scene tree for the first time.
+
 
 
 func _on_Button_pressed():
-	get_tree().change_scene("res://Betrayal.tscn")
+	get_tree().change_scene(scene_to_load)
