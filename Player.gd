ae8cd22f8af860c390bcbc0d5c97004addb1fdbb
BlakeWiscombe <2011264@schoolsnet.act.edu.au>
2022-09-08 13:02:44 +1000

    basic death added

--- Player.gd
+++ Player.gd
@@ -75,4 +75,5 @@ func _physics_process (delta):
 	# jump if we press the jump button and are standing on the floor
 	if Input.is_action_pressed("jump") and is_on_floor():
 		playerVelocity.y = jumpStrength
-	
+	if Global.player_health == 0:
+		get_tree().change_scene("res://Main menu.tscn")
