5d80655afa7cb36933610b8ff49dcb6e0f1167ee
BlakeWiscombe <2011264@schoolsnet.act.edu.au>
2022-10-22 10:12:45 +1100

    update

--- Damage.gd
+++ Damage.gd
@@ -6,10 +6,5 @@ extends CollisionShape
 # var b = "text"
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
 func take_damage(damage):
 	print("ouch")
